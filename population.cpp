0a656af0b764b426bad9eb6520283bb0123eb15e
cwh <254471214+cwhvccs@users.noreply.github.com>
2026-04-18 19:37:40 -0400

    Add project files.

---
+++ population.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int startingPopulation, days;
+    double dailyIncrease;
+
+    //starting population
+    cout << "Enter the starting number of organisms: ";
+    cin >> startingPopulation;
+    while (startingPopulation < 2) {
+        cout << "Starting population must be at least 2. Try again: ";
+        cin >> startingPopulation;
+    }
+
+    //daily increase (>= 0)
+    cout << "Enter the average daily population increase (in %): ";
+    cin >> dailyIncrease;
+    while (dailyIncrease < 0) {
+        cout << "Percentage cannot be negative. Try again: ";
+        cin >> dailyIncrease;
+    }
+
+    //number of days
+    cout << "Enter the number of days they will multiply: ";
+    cin >> days;
+    while (days < 1) {
+        cout << "Days must be at least 1. Try again: ";
+        cin >> days;
+    }
+
+    int population = startingPopulation;
+
+    cout << "\nDay\tPopulation\n";
+
+    for (int day = 1; day <= days; day++) {
+        cout << day << "\t" << population << endl;
+        population += population * (dailyIncrease / 100);
+    }
+
+    return 0;
+}
